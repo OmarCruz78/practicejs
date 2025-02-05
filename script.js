d3d21996df55082ff7b77709fa96d2926e932986
OmarCruz78 <harekrshnayoga@gmail.com>
2025-02-05 17:23:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function pickImage(){
-    options = ["bacon_in_the_woods.png", "munising.jpg","bacon_near_the_fort.png", "hollow_tree.png", "lake_view.JPG", "bend_in_the_road.png","BaconGallupPark.jpg"]
-    randomImg = "images/" + options[Math.floor(Math.random() * options.length)];
-    img = document.querySelector("#header_img")
+    options = ["https://i.ibb.co/0FQjtdV/descarga-10.png", "https://i.ibb.co/kGStWZp/descarga-8.png","https://i.ibb.co/SRQrfc0/descarga-7.png", "https://i.ibb.co/dQThbkY/descarga-6.png", "https://i.ibb.co/1JX0WDn/descarga-5.png"]
+    randomImg = options[Math.floor(Math.random() * options.length)];
+    img = document.querySelector("#imagennav")
     img.setAttribute("src",randomImg)
     img.setAttribute("alt","")
 
